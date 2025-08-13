419317048c7184ed38799a2888ae0608753d61e1
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-13 10:27:08 -0500

    Clase concreta, implementación CursoService,superclase CommonServiceImpl

---
+++ src/main/java/com/formacionntt/microservicios/app/cursos/models/services/CursoServiceImpl.java
@@ -0,0 +1,9 @@
+package com.formacionntt.microservicios.app.cursos.models.services;
+
+import com.formacionntt.microservicios.app.cursos.models.entity.Curso;
+import com.formacionntt.microservicios.app.cursos.models.repository.CursoRepository;
+import com.formacionntt.microservicios.commons.services.CommonServiceImpl;
+
+public class CursoServiceImpl extends CommonServiceImpl<Curso, CursoRepository> implements CursoService {
+
+}
