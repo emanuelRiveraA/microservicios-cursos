cb959ed34d392b82935f2c5948267233418df849
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-13 09:57:08 -0500

    Creamo la interface heredando de CommonService

---
+++ src/main/java/com/formacionntt/microservicios/app/cursos/models/services/CursoService.java
@@ -0,0 +1,8 @@
+package com.formacionntt.microservicios.app.cursos.models.services;
+
+import com.formacionntt.microservicios.app.cursos.models.entity.Curso;
+import com.formacionntt.microservicios.commons.services.CommonService;
+
+public interface CursoService extends CommonService<Curso> {
+
+}
