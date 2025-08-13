ef1aabb7fffd7f459d3c11fa92d4d40dfc6f6bcd
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-13 10:31:01 -0500

    se agrega la anotación service

--- src/main/java/com/formacionntt/microservicios/app/cursos/models/services/CursoServiceImpl.java
+++ src/main/java/com/formacionntt/microservicios/app/cursos/models/services/CursoServiceImpl.java
@@ -1,9 +1,12 @@
 package com.formacionntt.microservicios.app.cursos.models.services;
 
+import org.springframework.stereotype.Service;
+
 import com.formacionntt.microservicios.app.cursos.models.entity.Curso;
 import com.formacionntt.microservicios.app.cursos.models.repository.CursoRepository;
 import com.formacionntt.microservicios.commons.services.CommonServiceImpl;
 
+@Service
 public class CursoServiceImpl extends CommonServiceImpl<Curso, CursoRepository> implements CursoService {
 
 }
