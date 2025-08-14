cfcd420725a8551780368b8660d9fc38a35b5337
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-13 23:38:15 -0500

    se agrega la entidad EntityScan

--- src/main/java/com/formacionntt/microservicios/app/cursos/MicroserviciosCursosApplication.java
+++ src/main/java/com/formacionntt/microservicios/app/cursos/MicroserviciosCursosApplication.java
@@ -2,8 +2,11 @@ package com.formacionntt.microservicios.app.cursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.formacionntt.microservicios.commons.alumnos.models.entity",
+			 "com.formacionntt.microservicios.app.cursos.models.entity"})
 public class MicroserviciosCursosApplication {
 
 	public static void main(String[] args) {
