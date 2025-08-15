d5ca9dce1729d1596cd0d16a8e0d982dc2dff4ed
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-15 18:29:21 -0500

    Se agrega metodo especializado en servicioImpl

--- src/main/java/com/formacionntt/microservicios/app/cursos/models/services/CursoServiceImpl.java
+++ src/main/java/com/formacionntt/microservicios/app/cursos/models/services/CursoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.formacionntt.microservicios.app.cursos.models.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.formacionntt.microservicios.app.cursos.models.entity.Curso;
 import com.formacionntt.microservicios.app.cursos.models.repository.CursoRepository;
@@ -8,5 +9,12 @@ import com.formacionntt.microservicios.commons.services.CommonServiceImpl;
 
 @Service
 public class CursoServiceImpl extends CommonServiceImpl<Curso, CursoRepository> implements CursoService {
+
+	@Override
+	@Transactional(readOnly = true)
+	public Curso findCursoByAlumnoId(Long id) {
+		// TODO Auto-generated method stub
+		return repository.findCursoByAlumnoId(id);
+	}
 
 }
