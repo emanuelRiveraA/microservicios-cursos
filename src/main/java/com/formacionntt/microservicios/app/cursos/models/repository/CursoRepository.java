5f4c4349fe4a01a2037a8f4d071227f371699076
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-15 18:28:53 -0500

    Se agrega metodo especializado en repositorio

--- src/main/java/com/formacionntt/microservicios/app/cursos/models/repository/CursoRepository.java
+++ src/main/java/com/formacionntt/microservicios/app/cursos/models/repository/CursoRepository.java
@@ -1,9 +1,13 @@
 package com.formacionntt.microservicios.app.cursos.models.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.formacionntt.microservicios.app.cursos.models.entity.Curso;
 
 public interface CursoRepository extends CrudRepository<Curso, Long> {
+	
+	@Query("SELECT c FROM Curso c JOIN FETCH c.alumnos a WHERE a.id=?1")//c = objeto Curso a = se refiere lso alumnos
+	public Curso findCursoByAlumnoId(Long id);
 
 }
