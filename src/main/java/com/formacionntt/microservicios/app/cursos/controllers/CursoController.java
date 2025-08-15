786093f0773f9770531f748df082d98ec4d66f94
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2025-08-15 01:14:18 -0500

    Se agregan los metodos

--- src/main/java/com/formacionntt/microservicios/app/cursos/controllers/CursoController.java
+++ src/main/java/com/formacionntt/microservicios/app/cursos/controllers/CursoController.java
@@ -1,5 +1,6 @@
 package com.formacionntt.microservicios.app.cursos.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionntt.microservicios.app.cursos.models.entity.Curso;
 import com.formacionntt.microservicios.app.cursos.models.services.CursoService;
+import com.formacionntt.microservicios.commons.alumnos.models.entity.Alumno;
 import com.formacionntt.microservicios.commons.controllers.CommonController;
 
 @RestController
@@ -27,6 +29,37 @@ public class CursoController extends CommonController<Curso, CursoService> {
 		cursoBD.setNombre(curso.getNombre());
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(cursoBD));
+	}
+	
+	@PutMapping("/{id}/asignar-alumnos")
+	public ResponseEntity<?> asignarAlumnos(@RequestBody List<Alumno> alumnos, @PathVariable Long id){
+		Optional<Curso> optCurso = this.service.findById(id);
+		if (optCurso.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Curso cursoBD = optCurso.get();
+		//recorremos la lista de alumnos y se va a gregando al curso
+		alumnos.forEach(a -> {
+			cursoBD.addAlumno(a);
+		});
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(cursoBD));		
+	}
+	
+	
+	@PutMapping("/{id}/eliminar-alumno")
+	public ResponseEntity<?> eliminarAlumnos(@RequestBody Alumno alumno, @PathVariable Long id){
+		Optional<Curso> optCurso = this.service.findById(id);
+		if (optCurso.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Curso cursoBD = optCurso.get();
+		
+		cursoBD.removeAlumno(alumno);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(cursoBD));		
 	}
 
 }
